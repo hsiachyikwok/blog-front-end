ddd70c27d3a13731ef22568927ab0c94231a1fd6
hisachyikwok <hsiachyikwok@gmail.com>
2018-02-13 14:55:02 +0800

    404页面

--- src/views/404/index.vue
+++ src/views/404/index.vue
@@ -1,5 +1,23 @@
 <template>
-<div>
-  404 not found
-</div>
+<div id="svgContainer"></div>
 </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+#svgContainer {
+  width: 640px;
+  height: 512px;
+  background-color: white;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: auto;
+}
+</style>

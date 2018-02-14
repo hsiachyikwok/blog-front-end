35f6fcd23b00e079ce17402b1caca52e14fc97c1
hisachyikwok <hsiachyikwok@gmail.com>
2018-02-14 11:40:26 +0800

    404

--- src/views/404/index.vue
+++ src/views/404/index.vue
@@ -1,23 +1,4 @@
 <template>
-<div id="svgContainer"></div>
+<!-- 404 -->
+<div> 404 not found</div>
 </template>
-
-<script>
-export default {
-
-}
-</script>
-
-<style>
-#svgContainer {
-  width: 640px;
-  height: 512px;
-  background-color: white;
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  margin: auto;
-}
-</style>

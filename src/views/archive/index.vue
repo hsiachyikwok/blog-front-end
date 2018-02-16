482a4f8ade6642232a3c4c90ebba15a461e79d60
hisachyikwok <hsiachyikwok@gmail.com>
2018-02-16 23:35:11 +0800

    归档时间轴

--- src/views/archive/index.vue
+++ src/views/archive/index.vue
@@ -6,9 +6,15 @@
         <v-flex>
           <timeline>
             <div v-for="n in 3">
-              <timeline-title>title</timeline-title>
-              <timeline-item color="#9dd8e0">item1</timeline-item>
-              <timeline-item :hollow="true">item2</timeline-item>
+              <timeline-title>2018-2-16</timeline-title>
+              <timeline-item color="#9dd8e0">spring 源码分析</timeline-item>
+              <timeline-item :hollow="true">vue 入门</timeline-item>
+              <timeline-item :hollow="true">vue 入门</timeline-item>
+              <timeline-item :hollow="true">
+                <v-avatar>
+                  <img src="http://5b0988e595225.cdn.sohucs.com/images/20180205/05a9b6978c47476d8d12694de36dedfe.jpeg" alt="avatar">
+                </v-avatar>
+              </timeline-item>
             </div>
           </timeline>
         </v-flex>

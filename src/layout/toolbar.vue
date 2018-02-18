655a8486f7447075e4661161e099c2e27208f282
hisachyikwok <hsiachyikwok@gmail.com>
2018-02-18 11:36:27 +0800

    工具栏加文字

--- src/layout/toolbar.vue
+++ src/layout/toolbar.vue
@@ -11,7 +11,7 @@
     </v-list>
   </v-menu>
   <v-toolbar-title>
-    <router-link to="/">Hsia'Blog</router-link>
+    Hsia'Blog
   </v-toolbar-title>
   <v-flex ml-5>
     <router-link to="/index">
@@ -19,26 +19,31 @@
         <v-icon small>fa-h-square</v-icon>
       </v-btn>
     </router-link>
+    首页
     <router-link to="/archive">
       <v-btn icon>
         <v-icon small>fa-archive</v-icon>
       </v-btn>
     </router-link>
+    归档
     <router-link to="/tag">
       <v-btn icon>
         <v-icon small>fa-tags</v-icon>
       </v-btn>
     </router-link>
+    标签
     <router-link to="/category">
       <v-btn icon>
         <v-icon small>fa-cube</v-icon>
       </v-btn>
     </router-link>
+    分类
     <router-link to="/about">
       <v-btn icon>
         <v-icon small>fa-user-secret</v-icon>
       </v-btn>
     </router-link>
+    关于
   </v-flex>
   <v-spacer></v-spacer>
   <v-btn icon>
@@ -51,9 +56,11 @@
 <script>
 export default {
   name: 'toolbar',
-  data(){
-    return{
-      items:[{title:'管理'}]
+  data() {
+    return {
+      items: [{
+        title: '管理'
+      }]
     }
   },
   methods: {

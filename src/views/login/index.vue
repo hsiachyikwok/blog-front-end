e64aa1651aba7f22146035d66b480fbdafd2fac1
hisachyikwok <hsiachyikwok@gmail.com>
2018-02-20 14:36:01 +0800

    login

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,7 +4,7 @@
     <v-container fluid fill-height>
       <v-layout align-center justify-center>
         <v-flex xs12 sm8 md4>
-          <v-card class="elevation-12">
+          <v-card>
             <v-toolbar dark color="primary">
               <v-toolbar-title>登录</v-toolbar-title>
               <v-spacer></v-spacer>

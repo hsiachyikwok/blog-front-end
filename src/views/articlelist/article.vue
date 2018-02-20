1538bd62b62eacba36295add25ebb9072e5a82db
hisachyikwok <hsiachyikwok@gmail.com>
2018-02-20 10:11:41 +0800

    文章列表card抽成组件

---
+++ src/views/articlelist/article.vue
@@ -0,0 +1,52 @@
+<template>
+<v-card>
+  <v-card-title primary-title>
+    <v-layout column>
+      <v-flex text-xs-center>
+        <h2>手把手教你撸一个博客系统</h2>
+      </v-flex>
+      <v-container grid-list-md>
+        <v-layout>
+          <v-flex xs12 row>
+            <v-layout row>
+              <v-flex><i class="far fa-calendar-alt"></i></v-flex>
+              <v-flex>{{timestamp}}</v-flex>
+            </v-layout>
+          </v-flex>
+          <v-flex xs12 justify-center>
+            <v-layout row justify-center>
+              <v-flex><i class="far fa-calendar-alt"></i></v-flex>
+              <v-flex>1000</v-flex>
+            </v-layout>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-layout>
+  </v-card-title>
+  <v-card-text>
+    <vue-markdown :source="content"></vue-markdown>
+  </v-card-text>
+  <v-divider></v-divider>
+  <v-card-actions>
+    <v-icon small color="green">fa-tag</v-icon>
+    <a herf="www">springboot</a> ,
+    <a herf="www">vue</a> ,
+    <a herf="www">springcloud</a>
+    <v-spacer></v-spacer>
+    <v-btn flat color="orange" @click.native="$router.push({name: 'article', params: {id: 'springboot'}})">阅读更多</v-btn>
+  </v-card-actions>
+</v-card>
+</template>
+
+<script>
+export default {
+  name: 'articlecard',
+  data() {
+    return {
+      content: `  天门山，就是安徽当涂县的东梁山（古代又称博望山）与和县的西梁山的合称。两山夹江对峙，象一座天设的门户，形势非常险要，“天门”即由此得名。诗题中的“西岳峥嵘何壮哉！黄河如丝天际来。黄河万里触山动，盘涡毂转秦地雷。”“盘涡毂转”也就是“碧水东流至此回”，同样是描绘万里江河受到峥嵘奇险的山峰阻遏时出现的情景。绝句尚简省含蓄，所以不象七古那样写得淋漓尽致。`,
+      timestamp: '2018-2-13 18:23',
+      viewcount: '100'
+    }
+  }
+}
+</script>
--- src/views/articlelist/index.vue
+++ src/views/articlelist/index.vue
@@ -2,26 +2,7 @@
 <v-container>
   <v-layout column>
     <v-flex :key=n v-for="n in 3" mb-3 text-xs-left>
-      <v-card>
-        <v-card-title primary-title>
-          <v-flex text-xs-center>
-            <h2>手把手教你撸一个博客系统</h2>
-            <div><i class="far fa-calendar-alt"></i> {{timestamp}}</div>
-          </v-flex>
-        </v-card-title>
-        <v-card-text>
-          <vue-markdown :source="content"></vue-markdown>
-        </v-card-text>
-        <v-divider></v-divider>
-        <v-card-actions>
-          <v-icon small color="green">fa-tag</v-icon>
-          <a herf="www">springboot</a> ,
-          <a herf="www">vue</a> ,
-          <a herf="www">springcloud</a>
-          <v-spacer></v-spacer>
-          <v-btn flat color="orange" @click.native="$router.push({name: 'article', params: {id: 'springboot'}})">阅读更多</v-btn>
-        </v-card-actions>
-      </v-card>
+      <article-card></article-card>
     </v-flex>
     <v-flex>
       <v-pagination :length="5" v-model="page">
@@ -33,18 +14,17 @@
 
 <script>
 import VueMarkdown from 'vue-markdown'
+import ArticleCard from '@/views/articlelist/article'
 export default {
   name: 'articlelist',
   data() {
     return {
-      content: `  天门山，就是安徽当涂县的东梁山（古代又称博望山）与和县的西梁山的合称。两山夹江对峙，象一座天设的门户，形势非常险要，“天门”即由此得名。诗题中的“西岳峥嵘何壮哉！黄河如丝天际来。黄河万里触山动，盘涡毂转秦地雷。”“盘涡毂转”也就是“碧水东流至此回”，同样是描绘万里江河受到峥嵘奇险的山峰阻遏时出现的情景。绝句尚简省含蓄，所以不象七古那样写得淋漓尽致。`,
-      page: 1,
-      timestamp: '2018-2-13 18:23',
-      viewcount: '100'
+      page: 1
     }
   },
   components: {
-    VueMarkdown
+    VueMarkdown,
+    ArticleCard
   }
 }
 </script>

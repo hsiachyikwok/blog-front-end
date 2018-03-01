2a9bcbccc8ed96d5b97d52dda4659be4a3155e10
hisachyikwok <hsiachyikwok@gmail.com>
2018-03-01 13:08:54 +0800

    配置管理

--- src/views/admin/settingsmanage.vue
+++ src/views/admin/settingsmanage.vue
@@ -1,39 +1,62 @@
 <template>
-<v-container fluid fill-height>
-  <v-layout>
-    <v-form v-model="valid">
-      <v-text-field label="Name" v-model="name" :rules="nameRules" :counter="10" required></v-text-field>
-      <v-text-field label="E-mail" v-model="email" :rules="emailRules" required></v-text-field>
-      <v-text-field label="E-mail" v-model="email" :rules="emailRules" required></v-text-field>
-      <v-text-field label="E-mail" v-model="email" :rules="emailRules" required></v-text-field>
-      <v-text-field label="E-mail" v-model="email" :rules="emailRules" required></v-text-field>
-      <v-text-field label="E-mail" v-model="email" :rules="emailRules" required></v-text-field>
-      <v-text-field label="E-mail" v-model="email" :rules="emailRules" required></v-text-field>
-      <v-text-field label="E-mail" v-model="email" :rules="emailRules" required></v-text-field>
-      <v-text-field label="E-mail" v-model="email" :rules="emailRules" required></v-text-field>
-      <v-text-field label="E-mail" v-model="email" :rules="emailRules" required></v-text-field>
-      <v-text-field label="E-mail" v-model="email" :rules="emailRules" required></v-text-field>
-    </v-form>
+<v-container fluid grid-list-md text-xs-center>
+  <v-layout row>
+    <v-flex xs12>
+      <v-form v-model="valid">
+        <v-text-field label="头像" v-model="configForm.avatar" :rules="nameRules"></v-text-field>
+        <v-text-field label="博客标题" v-model="configForm.blogTitle" :rules="nameRules"></v-text-field>
+        <v-text-field label="博客子标题" v-model="configForm.blogSubTitle" :rules="nameRules"></v-text-field>
+        <v-text-field label="博客logo" v-model="configForm.blogLogo" :rules="nameRules"></v-text-field>
+        <v-text-field label="favicon" v-model="configForm.favicon" :rules="nameRules"></v-text-field>
+        <v-text-field label="MetaKeyWords" v-model="configForm.metaKeywords" :rules="nameRules"></v-text-field>
+        <v-text-field label="MetaDesc" v-model="configForm.metaDesc" :rules="nameRules"></v-text-field>
+        <v-text-field label="Footer" v-model="configForm.footer" :rules="nameRules"></v-text-field>
+        <v-text-field label="公告" v-model="configForm.notice" :rules="nameRules"></v-text-field>
+        <v-text-field label="允许评论" v-model="configForm.allowComment" :rules="nameRules"></v-text-field>
+        <v-text-field label="文章列表样式" v-model="configForm.listStyle" :rules="nameRules"></v-text-field>
+        <v-text-field label="最近评论数" v-model="configForm.recentCommentSize" :rules="nameRules"></v-text-field>
+        <v-text-field label="分页大小" v-model="configForm.pageSize" :rules="nameRules"></v-text-field>
+        <v-text-field label="分页宽度" v-model="configForm.pageWidth" :rules="nameRules"></v-text-field>
+        <v-text-field label="最多浏览文章显示数" v-model="configForm.mostViewSize" :rules="nameRules"></v-text-field>
+        <v-text-field label="最多评论显示数" v-model="configForm.mostCommentSize" :rules="nameRules"></v-text-field>
+        <v-text-field label="待选标签显示数" v-model="configForm.usedTagSize" :rules="nameRules"></v-text-field>
+        <v-text-field label="位置" v-model="configForm.location" :rules="nameRules"></v-text-field>
+        <v-text-field label="关于" v-model="configForm.about" :rules="nameRules"></v-text-field>
+      </v-form>
+    </v-flex>
+    <v-flex xs12>
+      <v-btn @click="updateConfig()">更新</v-btn>
+    </v-flex>
   </v-layout>
 </v-container>
 </template>
 
 <script>
 import api from '@/api'
+import storage from '@/utils/storage.js'
 export default {
   name: 'settingsmanage',
-  data: () => ({
-    valid: false,
-    name: '',
-    nameRules: [
-      v => !!v || 'Name is required',
-      v => v.length <= 10 || 'Name must be less than 10 characters'
-    ],
-    email: '',
-    emailRules: [
-      v => !!v || 'E-mail is required',
-      v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
-    ]
-  })
+  data() {
+    return {
+      valid: false,
+      configForm: {},
+      nameRules: [
+        v => !!v || 'Value is required',
+        // v => v.length <= 10 || 'Name must be less than 10 characters'
+      ],
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        // v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
+      ]
+    }
+  },
+  methods: {
+    updateConfig() {
+
+    }
+  },
+  mounted() {
+    this.configForm = storage.get('config')
+  }
 }
 </script>

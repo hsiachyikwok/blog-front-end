8a6a4633020be611250a902be273392b2d5c7608
hisachyikwok <hsiachyikwok@gmail.com>
2018-03-05 01:28:27 +0800

    后台动态路由

--- src/router/index.js
+++ src/router/index.js
@@ -5,16 +5,16 @@ import article from '@/views/article'
 import error from '@/views/404'
 import tag from '@/views/tag'
 import category from '@/views/category'
-import admin from '@/views/admin'
+// import admin from '@/views/admin'
 import about from '@/views/about'
 import articlelist from '@/views/articlelist'
 import archive from '@/views/archive'
 import login from '@/views/login'
-import articlemanage from '@/views/admin/articlemanage'
-import settingsmanage from '@/views/admin/settingsmanage'
-import commentmanage from '@/views/admin/commentmanage'
-import draftbox from '@/views/admin/draftbox'
-import postarticle from '@/views/admin/postarticle'
+// import articlemanage from '@/views/admin/articlemanage'
+// import settingsmanage from '@/views/admin/settingsmanage'
+// import commentmanage from '@/views/admin/commentmanage'
+// import draftbox from '@/views/admin/draftbox'
+// import postarticle from '@/views/admin/postarticle'
 import catarticlelist from '@/views/category/catarticle'
 import tagarticlelist from '@/views/tag/tagarticle'
 
@@ -88,47 +88,47 @@ export default new Router({
         },
       ]
     },
-    {
-      path: '/admin',
-      component: admin,
-      children: [{
-          path: '/admin/articlemanage',
-          component: articlemanage,
-          meta: {
-            title: '文章管理'
-          }
-        },
-        {
-          path: '/admin/settingsmanage',
-          component: settingsmanage,
-          meta: {
-            title: '设置管理'
-          }
-        },
-        {
-          path: '/admin/draftbox',
-          component: draftbox,
-          meta: {
-            title: '草稿箱'
-          }
-        },
-        {
-          path: '/admin/commentmanage',
-          component: commentmanage,
-          meta: {
-            title: '评论管理'
-          }
-        },
-        {
-          path: '/admin/postarticle',
-          component: postarticle,
-          name: 'postarticle',
-          meta: {
-            title: '文章管理'
-          }
-        },
-      ]
-    },
+    // {
+    //   path: '/admin',
+    //   component: admin,
+    //   children: [{
+    //       path: '/admin/articlemanage',
+    //       component: articlemanage,
+    //       meta: {
+    //         title: '文章管理'
+    //       }
+    //     },
+    //     {
+    //       path: '/admin/settingsmanage',
+    //       component: settingsmanage,
+    //       meta: {
+    //         title: '设置管理'
+    //       }
+    //     },
+    //     {
+    //       path: '/admin/draftbox',
+    //       component: draftbox,
+    //       meta: {
+    //         title: '草稿箱'
+    //       }
+    //     },
+    //     {
+    //       path: '/admin/commentmanage',
+    //       component: commentmanage,
+    //       meta: {
+    //         title: '评论管理'
+    //       }
+    //     },
+    //     {
+    //       path: '/admin/postarticle',
+    //       component: postarticle,
+    //       name: 'postarticle',
+    //       meta: {
+    //         title: '编辑文章'
+    //       }
+    //     },
+    //   ]
+    // },
     {
       path: '/404',
       component: error,

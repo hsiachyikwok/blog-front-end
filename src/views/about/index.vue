dbc4297a6d00e17070304a2ff19fc02594db242e
hisachyikwok <hsiachyikwok@gmail.com>
2018-03-05 10:50:04 +0800

    剩下前端表单校验和代码高亮

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -5,7 +5,7 @@
       <v-card height="600px">
         <v-flex py-3 px-3>
           <v-card-text>
-            <vue-markdown v-highlightjs :source="this.about"></vue-markdown>
+            <vue-markdown :source="this.about"></vue-markdown>
           </v-card-text>
         </v-flex>
       </v-card>
